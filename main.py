89fc63babb5fc375cad6648b025049f59cbc2cc6
sven.verstegen <sven.verstegen@paccar.com>
2025-10-29 15:45:01 +0100

    change strings

--- main.py
+++ main.py
@@ -18,11 +18,38 @@ async def on_message(message: discord.Message):
 
     if bot.user.mentioned_in(message):
         responses = [
-            "Hello!",
-            "Hi there!",
-            "Greetings!",
-            "Salutations!",
-            "Howdy!"
+            "Ja, deze poost is gefeitencheckt door echte idioten",
+            "Ja, deze poost is gefeitencheckt door echte patrioten",
+            "Ja, dit gaat hard",
+            "Ja, mijn bronnen zeggen mij van wel",
+            "Wij gaan jou jou doodslaan",
+            "Ik denk het wel",
+            "Volgens mij is dit waar",
+            "Volgens mij is dit wanneer",
+            "Volgens mij is dit waarom",
+            "Hoe durf je dit te vragen",
+            "Bobbie jij soepkip je hebt Grok gevraagd",
+            "Moet je niet aan mij vragen",
+            "Ik weet wat je vraagt maar ik zeg het lekker niet",
+            "Heb ik geen zin in",
+            "Ik wordt daar zo moe van van zeggen of het waar is, ik doe niet meer mee, ik ga slapen",
+            "The Bluetooth Device is Ready To Pair",
+            "Waarom vraag je mij?",
+            "Vraag het aan @pika1106 ofzo.",
+            "Wat een ongeloofelijke kutvraag",
+            "Nee, dit is Grok",
+            "Dit is niet echt.",
+            "Dit is nep",
+            "Dit hebben de mensen van de redactie helemaal zelf bedacht",
+            "Dit hebben we verzonnen",
+            "Waarom zou je ooit denken dat dit echt was?",
+            "Lees de vraag eens hardop",
+            "Waarom zijn de bananen grok?",
+            "Jou Grok rechten zijn bij deze afgenomen.",
+            "Nee dit is niet echt.",
+            "Voor de zoveelste keer, nee.",
+            "Hak een boom om met een haring en vraag het nog eens.",
+            "Kun je die vraag herhalen? Ik begreep het niet helemaal."
         ]
         await message.channel.send(random.choice(responses))
 
